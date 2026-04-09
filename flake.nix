4593e47f8b467449e12c21a08b59407bebc406bb
Mike Clarke <clarkema@clarkema.org>
2026-04-09 20:34:39 +0100

    Add git-map flake

--- flake.nix
+++ flake.nix
@@ -3,26 +3,39 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    git-map = {
+      url = "github:clarkema/git-map";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      git-map,
+    }:
     let
-      forAllSystems = function:
+      forAllSystems =
+        function:
         nixpkgs.lib.genAttrs [
           "x86_64-linux"
           "x86_64-darwin"
           "aarch64-linux"
           "aarch64-darwin"
         ] (system: function nixpkgs.legacyPackages.${system});
-    in {
-      packages = forAllSystems(pkgs:
+    in
+    {
+      packages = forAllSystems (
+        pkgs:
         let
-          pcal  = import ./pcal.nix { inherit pkgs; };
-          tm  = import ./tm.nix { inherit pkgs; };
+          pcal = import ./pcal.nix { inherit pkgs; };
+          tm = import ./tm.nix { inherit pkgs; };
         in
         {
           pcal = pcal;
           tm = tm;
+          git-map = git-map.packages.${pkgs.system}.git-map;
         }
       );
     };
